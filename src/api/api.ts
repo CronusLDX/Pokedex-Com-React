1bed1d7e1f17fe5d8c9b7e41f2adeaab064638c1
João Pedro Barbosa Duarte Ferreira <141972472+CronusLDX@users.noreply.github.com>
2025-02-24 15:57:38 -0300

    fix retirei o .env

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: 'https://pokeapi.co/api/v2/',
   timeout: 1000,
   headers: {
     'Content-Type': 'application/json',
